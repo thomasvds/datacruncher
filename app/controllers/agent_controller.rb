182255292f3ec68c7515ecc8b239dd1a96c69cb1
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-07-01 17:40:04 +0200

    Nicer score labels

--- app/controllers/agent_controller.rb
+++ app/controllers/agent_controller.rb
@@ -10,12 +10,16 @@ class AgentController < ApplicationController
 
     last_week_events = Event.where(agent: @agent, date: date_range)
 
-    @policies_check_list, @score = Policy.check_list(last_week_events)
+    @policies_check_list,
+    @week_score, @week_score_range = Policy.check_list(last_week_events)
 
     @list_of_dates = date_range.map {|d| d.strftime '%a %d/%m' }
     @hour_of_last_event_by_date = Event.hour_of_last_event_by_date(@agent, date_range)
 
-    @list_of_weeks, @all_time_score = all_time_score
+    @list_of_weeks,
+    @score_by_week,
+    @current_average_score,
+    @current_average_score_range = Policy.all_time_scores(@agent)
   end
 
   private
@@ -28,40 +32,4 @@ class AgentController < ApplicationController
     params.require(:agent).permit(:id)
   end
 
-  def all_time_score
-    start_date  = Date.parse('2016-04-01')
-    end_date    = Date.parse('2016-06-26')
-
-    agent_events = Event.where(agent: @agent)
-
-    start_week = start_date.cweek
-    end_week = end_date.cweek
-
-    week_xaxis = start_week..end_week
-    scores = []
-
-    week = start_week
-
-    while week <= end_week
-      wkstart = Date.commercial(2016, week, 1)
-      wkend = Date.commercial(2016, week, 7)
-      date_range = wkstart..wkend
-      events = agent_events.where(date: date_range)
-      policies_check_list, score = Policy.check_list(agent_events.where(date: date_range))
-      scores << score
-      week += 1
-    end
-
-    averages = []
-    averages[0] = scores[0]
-    averages[1] = 0.5 * (scores[0] + scores[1])
-    averages[2] = 0.33333 * (scores[0] + scores[1] + scores[2])
-    (3...scores.count).each do |i|
-      averages[i] = 0.25 * (scores[i-3] + scores[i-2] + scores[i-1] + scores[i])
-    end
-
-
-
-    return week_xaxis, {"weekly" => scores, "4-wks avg." => averages}
-  end
 end
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -10,7 +10,45 @@ class Policy < ActiveRecord::Base
       score += policy.weight * increment
     end
     score = (score * 100).round
-    return check_list_output, score
+
+    return check_list_output, score, score_range(score)
+  end
+
+  def self.all_time_scores(agent)
+    start_date  = Date.parse('2016-04-01')
+    end_date    = Date.parse('2016-06-26')
+
+    agent_events = Event.where(agent: agent)
+
+    start_week = start_date.cweek
+    end_week = end_date.cweek
+
+    week_xaxis = start_week..end_week
+    scores = []
+
+    week = start_week
+
+    while week <= end_week
+      wkstart = Date.commercial(2016, week, 1)
+      wkend = Date.commercial(2016, week, 7)
+      date_range = wkstart..wkend
+      events = agent_events.where(date: date_range)
+      policies_check_list, score = check_list(agent_events.where(date: date_range))
+      scores << score
+      week += 1
+    end
+
+    averages = []
+    averages[0] = scores[0]
+    averages[1] = 0.5 * (scores[0] + scores[1])
+    averages[2] = 0.33333 * (scores[0] + scores[1] + scores[2])
+    (3...scores.count).each do |i|
+      averages[i] = 0.25 * (scores[i-3] + scores[i-2] + scores[i-1] + scores[i])
+    end
+
+    current_average_score = averages[-1].round
+
+    return week_xaxis, {"weekly" => scores, "4-wks avg." => averages}, current_average_score, score_range(current_average_score)
   end
 
   def enforced?(events)
@@ -49,6 +87,19 @@ class Policy < ActiveRecord::Base
       return !Event.any_event_after?(events, self.hour)
     when "at all"
       return events.count == 0
+    end
+  end
+
+  def self.score_range(score)
+    case score
+    when 0..40
+      "danger"
+    when 41..60
+      "warning"
+    when 61..80
+      "primary"
+    else
+      "success"
     end
   end
 end
