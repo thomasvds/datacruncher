5f1ff8d60d3fb12af7e702e63a237850ce79ff35
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-28 13:14:08 +0200

    csv clean

--- app/jobs/trello_job.rb
+++ app/jobs/trello_job.rb
@@ -23,49 +23,28 @@ csv_options = {col_sep: ',', write_headers: false,
     'hour',
     'minute']}
 
-CSV.open(filename, 'w', csv_options) do |csv|
-  json['cards'].each do |card|
-    source = "trello"
-    source_channel = ""
-    card_time = card['dateLastActivity'][0...-5]
-    time = DateTime.strptime("#{card_time}", '%Y-%m-%dT%H:%M:%S')
-    date = time.strftime('%Y%m%d')
-    hour = time.strftime('%H')
-    minute = time.strftime('%M')
-    source_agent_id = card['id']
-    extraction_time = Time.now
-    agent_id = 'nil'
-    category = "production"
-    csv << [source,
-    source_channel,
-    source_agent_id,
-    extraction_time,
-    agent_id, category,
-    time, date, hour, minute]
-  end
-end
-
-
-
-json['cards'].size
-
-# CSV.open(@filename, "w", csv_options) do |csv| #open new file for write
-#   json.each do |event| #open json to parse
-#     #Extract event characteristics
-#     time = event['created_at']
-#     #TODO: use a real datetime object instead of github created_at
-#     time_match = time.scan(/[^A-Z:]*/)
-#     source_agent_id = event['actor']['id']
-#     extraction_time = extracted
-#     agent_id = Agent.where(github_id: source_agent_id).first.id
-#     category = 'production'
-#     date = time_match[0]
-#     hour = time_match[2]
-#     minute = time_match[4]
-#     #Append event as new row to CSV file
-#     csv << ['github',
-#     @channel,
+# CSV.open(filename, 'w', csv_options) do |csv|
+#   json['cards'].each do |card|
+#     source = "trello"
+#     source_channel = ""
+#     card_time = card['dateLastActivity'][0...-5]
+#     time = DateTime.strptime("#{card_time}", '%Y-%m-%dT%H:%M:%S')
+#     date = time.strftime('%Y%m%d')
+#     hour = time.strftime('%H')
+#     minute = time.strftime('%M')
+#     source_agent_id = card['id']
+#     extraction_time = Time.now
+#     agent_id = 'nil'
+#     category = "production"
+#     csv << [source,
+#     source_channel,
 #     source_agent_id,
 #     extraction_time,
 #     agent_id, category,
 #     time, date, hour, minute]
+#   end
+# end
+
+
+
+json['cards'].size
