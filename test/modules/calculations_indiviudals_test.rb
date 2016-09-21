4363816cd6a27d759f39bc67171f6a26c51e8f97
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-09-21 12:28:49 +0200

    Built the testing file

---
+++ test/modules/calculations_indiviudals_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+include Calculations::Individuals
+
+# To run this specific test only, run:
+# rake test TEST=test/modules/calculations_individuals_test.rb
+
+class CalculationsIndividualsTest < ActiveSupport::TestCase
+end
