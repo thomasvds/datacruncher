22dcb1b849d50ffb2a6a6a5d46a363a5c93b51fb
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-09-22 17:29:49 +0200

    Finished test of score boxes and policy gauges

--- test/integration/report_company_test.rb
+++ test/integration/report_company_test.rb
@@ -13,25 +13,53 @@ class ReportCompanyTest < ActionDispatch::IntegrationTest
   test "displays one score box per range" do
     visit 'reports/company'
     Calculations::RANGE.each do |range_name, range_values|
-      page.should have_css("panel-#{range_name}", :count => 2)
+      assert page.has_selector?(".panel-#{range_name}", count: 1)
     end
   end
 
-  # test "current score range figures add up to number of employees in company" do
-  #   visit 'reports/company'
-  # end
+  test "current score range figures add up to number of employees in company" do
+    visit 'reports/company'
+    sum = 0
+    Calculations::RANGE.each do |range_name, range_values|
+      sum += page.first(".panel-#{range_name}").first(".huge").text.to_i
+    end
+    assert_equal Agent.all.count , sum
+  end
 
-  # test "previous week score range figures add up to number of employees in company" do
-  #   visit 'reports/company'
-  # end
+  test "previous week score range figures add up to number of employees in company" do
+    visit 'reports/company'
+    sum = 0
+    Calculations::RANGE.each do |range_name, range_values|
+      content = page.first(".panel-#{range_name}").first(".pull-left").text
+      value = content.match(/\d+/)[0].to_i
+      sum += value
+    end
+    assert_equal Agent.all.count , sum
+  end
 
-  # test "displays one gauge per policy, for enabled policies only" do
-  #   visit 'reports/company'
-  # end
+  test "displays one gauge for each enabled policy" do
+    visit 'reports/company'
+    Policy.enabled.each do |policy|
+      assert page.first(".panel-default").has_content?(policy.name)
+    end
+  end
 
-  # test "policy scores are all comprised between 0 and 100%" do
-  #   visit 'reports/company'
-  # end
+  test "does not display gauges for policies that are not enabled" do
+    visit 'reports/company'
+    Policy.enabled.each do |policy|
+      assert page.first(".panel-default").has_content?(policy.name)
+    end
+    assert page.first(".panel-default").has_selector?(".highcharts-container", count: Policy.enabled.count)
+  end
+
+  test "gauge policy scores are all comprised between 0 and 100%" do
+    visit 'reports/company'
+    page.first(".panel-default").all(".highcharts-container").each do |chart|
+      content =  chart.first("span").text
+      value = content.match(/\d+/)[0].to_i
+      assert_includes 0..100, value
+    end
+  end
 
   # test "displays table with one row per individual" do
   #   visit 'reports/company'
