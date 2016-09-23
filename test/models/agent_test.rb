86735ce261eeb11e3792a716b144ab4c390e6fcf
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-09-23 11:08:07 +0200

    checking enabled? method

--- test/models/agent_test.rb
+++ test/models/agent_test.rb
@@ -12,6 +12,15 @@ class AgentTest < ActiveSupport::TestCase
     assert_includes agent.errors.messages[:name], "can't be blank"
   end
 
+  test "info_hash returns hash with name, id, position and picture_url" do
+    agent = Agent.create(@params)
+    result = agent.info_hash
+    assert_includes result, :name
+    assert_includes result, :id
+    assert_includes result, :position
+    assert_includes result, :picture_url
+  end
+
   test "agent is invalid when invalid param" do
     @params.merge!(invalid_key: nil)
     assert_raises(ActiveRecord::UnknownAttributeError) { Agent.create(@params) }
--- test/models/policy_test.rb
+++ test/models/policy_test.rb
@@ -12,6 +12,14 @@ class PolicyTest < ActiveSupport::TestCase
     assert_includes policy.errors.messages[:name], "can't be blank"
   end
 
+  test "instance method enabled? check whether policy enables" do
+    policy = Policy.create(@params)
+    enabled_policy = PolicySetting.create(policy: policy, enabled: true)
+    assert_equal enabled_policy.enabled?, true
+    disabled_policy = PolicySetting.create(policy: policy, enabled: false)
+    assert_not_equal disabled_policy.enabled?, true
+  end
+
   test "policy is invalid when invalid param" do
     @params.merge!(invalid_key: nil)
     assert_raises(ActiveRecord::UnknownAttributeError) { Policy.create(@params) }
